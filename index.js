0893326b45e3dfe4959ae1b1057c728cff88b7b2
Xenia Wilczek <xenia.wilczek@gmx.de>
2026-01-31 16:44:26 +0100

    Adapt styling of food-drinks-items and border of ordered products, change logic for delivery button

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-// Variable für Zähler mit Initialwert 0
+//Funktion für Addition des Preises mit Variable count
 let count = 0;
-// Parameter price für aktuell dazugezählten Preis (in HTML eintragen)
+// Parameter price für aktuell dazugezählten Preis, auch Parametername für Produktnamen
 function addPrice(name, price) {
   //Nun Gesamtpreis berechnen und anzeigen
   count = count + price;
@@ -9,22 +9,22 @@ function addPrice(name, price) {
   showOrder(name, price);
   displayTotalPrice();
 }
-
+//Funktion zur Anzeige des Gesamtpreises
 function displayTotalPrice() {
   const display = document.getElementById("total-price");
   display.innerHTML = count;
   console.log(count);
 }
-
+//Funktion zur Anzeige der bestellten Produkte (rechts aufgelistet)
 function showOrder(name, price) {
   const show = document.getElementById("order-list");
   show.innerHTML += `<p> ${name}, ${price} €</p>`;
   console.log(name, price);
 }
 
+//Funktion zum Zurücksetzen des Gesamtpreises und der Liste aufgezählter Produkte
 function reset() {
   //Zurücksetzen-Button ansprechen über id
-  const refreshDisplay = document.getElementById("upper-button");
   //Wenn  Gesamtpreis = 0, kommt Hinweis: Produkte auswählen!
   if (count === 0) {
     alert("Bitte zuerst Artikel auswählen!");
@@ -57,16 +57,15 @@ function payDelivery() {
   if (count === 0) {
     alert("Bitte zuerst Artikel auswählen!");
     //ansonsten: Wenn Produkte ausgewählt wurden...
-    //..und der Betrag <20€, dann Hinweis auf Mindestbetrag
+    //..und der Betrag <20€, dann Hinweis auf Mindestbetrag für Bestellung
   } else if (count < 20) {
     displayTotalPrice();
-    reset();
     alert("Mindestbetrag für Lieferung: 20€!");
     //Wenn Betrag>=20€, dann 2,50€ hinzuzählen als Liefergebühr
   } else {
     count = count + 2.5;
     displayTotalPrice();
-    alert("Gesamtpreis inkl. 2.50€ Liefergebühr: " + count);
+    alert("Gesamtpreis inkl. 2.50€ Liefergebühr: " + count + " €");
     reset();
   }
 }
